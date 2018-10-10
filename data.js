9d93897568101f98594d845f722b2f35b05dadd2
Nate Clark <natec425@gmail.com>
2018-10-10 10:15:39 -0500

    Add started PAGE_DATA
    I manually entered the data from what I could see from the twbs/bootstrap
    projects page.
    
    I tried to structure it in a way that would be reasonable for the students
    to adapt their existing HTML to.

---
+++ data.js
@@ -0,0 +1,106 @@
+const PAGE_DATA = {
+  currentUser: {
+    username: "natec425",
+    avatarUrl: "https://avatars1.githubusercontent.com/u/1424988?s=40&v=4"
+  },
+  repository: {
+    numWatchers: 7394,
+    numStars: 127880,
+    numForks: 62508,
+    owner: {
+      name: "twbs"
+    },
+    name: "bootstrap",
+    issues: {
+      amountOpen: 378
+    },
+    pullRequests: {
+      amountOpen: 130
+    },
+    projects: [
+      {
+        title: "v4.1.3",
+        closed: true,
+        lastUpdated: new Date(2018, 6, 24),
+        progress: {
+          totalTasks: 44,
+          tasksFinished: 44
+        }
+      },
+      {
+        title: "v4.1.2",
+        closed: true,
+        lastUpdated: new Date(2018, 6, 12),
+        progress: {
+          totalTasks: 59,
+          tasksFinished: 59
+        }
+      },
+      {
+        title: "v4.1.1",
+        closed: true,
+        lastUpdated: new Date(2018, 3, 30),
+        description:
+          "Patch release following v4.1 to pick up some remaining pieces \
+                      before moving onto new or updated features.",
+        progress: {
+          totalTasks: 59,
+          tasksFinished: 59
+        }
+      },
+      {
+        title: "v4.0 stable",
+        closed: true,
+        lastUpdated: new Date(2018, 0, 18),
+        progress: {
+          totalTasks: 99,
+          tasksFinished: 99
+        }
+      },
+      {
+        title: "v5 ideas",
+        closed: false,
+        lastUpdated: new Date(2018, 0, 12),
+        description:
+          "Don't get too excited -- I'm just capturing random ideas here."
+      },
+      {
+        title: "v4 Beta 3",
+        closed: true,
+        lastUpdated: new Date(2017, 11, 28),
+        progress: {
+          totalTasks: 161,
+          tasksFinished: 161
+        }
+      },
+      {
+        title: "v4.3",
+        closed: false,
+        lastUpdated: new Date(2018, 8, 30),
+        description:
+          "Publish each component as it's own repository with Lerna.",
+        progress: {
+          totalTasks: 1,
+          tasksFinished: 0
+        }
+      },
+      {
+        title: "Feature ideas",
+        closed: false,
+        lastUpdated: new Date(2018, 7, 25),
+        description:
+          "Catch-all project board for organizing feature requests we recieve from folks."
+      },
+      {
+        title: "v4.2",
+        closed: false,
+        lastUpdated: new Date(2018, 9, 7),
+        description: "Forms and component enhancements",
+        progress: {
+          totalTasks: 114,
+          tasksFinished: 111
+        }
+      }
+    ]
+  }
+};
